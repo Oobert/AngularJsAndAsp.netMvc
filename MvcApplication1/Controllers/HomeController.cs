a8afd4b8a9931cf261018beae1422396557627b8
Tony <noobert@gmail.com>
2012-10-01 12:11:21 -0500

    Init Check in.

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return PartialView();
+        }
+
+
+        public ActionResult AppOneIndex()
+        {
+            return View();
+        }
+
+        [OutputCache(Duration = 10)]
+        public ActionResult AppOne()
+        {
+            return PartialView();
+        }
+
+        [OutputCache(Duration = 10)]
+        public ActionResult AppOnePartTwo()
+        {
+            return PartialView();
+        }
+
+        public ActionResult AppTwoIndex()
+        {
+            return View();
+        }
+
+
+        [OutputCache(Duration = 10)]
+        public ActionResult AppTwo()
+        {
+            return PartialView();
+        }
+
+        [OutputCache(Duration = 10)]
+        public ActionResult AppTwoPartTwo()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ MvcApplication1/Scripts/index.js
@@ -0,0 +1,48 @@
+﻿angular.module('appOne', [], function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/AppOne/', { templateUrl: '/home/appone', controller: "appOneController" })
+                  .when('/AppOne/Two', { templateUrl: '/home/apponeparttwo', controller: "appOnePartTwoController" })
+                  .otherwise({ redirectTo: function (x, path, y) {
+                      window.location.href = path;
+                  }});
+
+    // configure html5 to get links working on jsfiddle
+    $locationProvider.html5Mode(true);
+});
+
+
+function appOneController($scope) {
+
+    $scope.helloWorld = "appOneController";
+
+}
+
+function appOnePartTwoController($scope) {
+
+    $scope.helloWorld = "appOnePartTwoController";
+
+}
+
+
+
+angular.module('appTwo', [], function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/AppTwo/', { templateUrl: '/home/apptwo', controller: "appTwoController" })
+                  .when('/AppTwo/Two', { templateUrl: '/home/apptwoparttwo', controller: "appTwoPartTwoController" })
+                  .otherwise({ redirectTo: function (x, path, y) {
+                      window.location.href = path;
+                  }});
+    // configure html5 to get links working on jsfiddle
+    $locationProvider.html5Mode(true);
+});
+
+
+function appTwoController($scope) {
+
+    $scope.helloWorld = "appTwoController";
+
+}
+
+function appTwoPartTwoController($scope) {
+
+    $scope.helloWorld = "appTwoPartTwoController";
+
+}
